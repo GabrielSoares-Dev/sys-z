9c9006c355216b45d2de58a6a4d09b3a4bcc6255
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-03-07 14:04:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sys-z
+
+npm i para instalar dependências
+
+npm run dev para rodar o projeto
---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,9 @@
+import './styles/global.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center">
+     <h1 className="font-semibold">Hello group</h1>
+    </div>
+  )
+}
