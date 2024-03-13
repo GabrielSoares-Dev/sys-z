194d3068944ec231cd29aaf71ff5fe6f7459293b
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-03-13 19:45:01 -0300

    provision data and install styled components

--- src/renderer/src/pages/dashboard/hooks/useDashboard/index.jsx
+++ src/renderer/src/pages/dashboard/hooks/useDashboard/index.jsx
@@ -1,44 +1,63 @@
-
 import { useEffect, useState } from 'react'
-import { convertBytes } from '../../../../utils';
-
+import { convertBytes } from '../../../../utils'
 
 export function useDashboard() {
-    const [systemData, setSystemData] = useState({
-        memoryData: {
-            available: 0,
-            used: 0,
-        },
-        cpuUsage: 0.00
+  const [systemData, setSystemData] = useState({
+    cpu: {
+      name: '',
+      usage: 0,
+      temperature: 0
+    },
+    gpu: {
+      name: '',
+      usage: 0,
+      temperature: 0
+    },
+    memory: {
+      available: 0,
+      used: 0,
+      types: []
+    }
+  })
+
+  const requestSystemInfo = () => window.electron.ipcRenderer.send('request-system-info')
+
+  const getSystemData = () => {
+    window.electron.ipcRenderer.on('system-info-data', (event, data) => {
+      const cpu = {
+        name: data.cpu.name,
+        usage: data.cpu.usage,
+        temperature: data.cpu.temperature
+      }
+
+      const memory = {
+        available: convertBytes(data.memory.available),
+        used: convertBytes(data.memory.used),
+        types: data.memory.types
+      }
+
+      const gpu = {
+        name: data.gpu.name,
+        usage: data.gpu.usage,
+        temperature: data.gpu.temperature
+      }
+
+      setSystemData({ cpu, gpu, memory })
     })
-    const requestSystemInfo = () => window.electron.ipcRenderer.send('request-system-info')
+  }
 
-    const getSystemData = () => {
-        window.electron.ipcRenderer.on('system-info-data', (event, data) => {
-            const memoryData = {
-                available: convertBytes(data.memoryData.available),
-                used: convertBytes(data.memoryData.used),
-            }
+  useEffect(() => {
+    getSystemData()
 
-            const cpuUsage = data.cpuUsage
+    const requestSystemInfoInterval = setInterval(requestSystemInfo, 5000)
 
-            setSystemData({ memoryData, cpuUsage })
-        });
+    return () => {
+      window.electron.ipcRenderer.removeAllListeners('system-info-data')
+      clearInterval(requestSystemInfoInterval)
     }
+  }, [])
 
-    useEffect(() => {
-        getSystemData()
-
-        const requestSystemInfoInterval = setInterval(requestSystemInfo, 5000);
-
-        return () => {
-            window.electron.ipcRenderer.removeAllListeners('system-info-data');
-            clearInterval(requestSystemInfoInterval);
-        };
-    }, [])
-
-
-    return {
-        systemData
-    }
+  return {
+    systemData
+  }
 }
